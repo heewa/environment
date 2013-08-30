32edd05a04235dbbe1242fd9b18009bf32d9e6ab
Heewa Barfchin <heewa.b@gmail.com>
2013-08-29 23:38:00 -0400

    Install script.

---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Set up symlinks to dotfiles & make sure directories I want are there.
+
+ln -vsf $PWD/dotfiles/vim ~/.vim
+ln -vsf $PWD/dotfiles/vim/vimrc ~/.vimrc
+ln -vsf $PWD/dotfiles/screenrc ~/.screenrc
+ln -vsf $PWD/dotfiles/gitconfig ~/.gitconfig
+ln -vsf $PWD/dotfiles/bashrc ~/.bashrc
+ln -vsf $PWD/dotfiles/profile ~/.profile
+
+for D in src build tmp scripts; do
+    mkdir -v $HOME/$D
+done
+
+# Create dropbox symlinks, even if Dropbox isn't updated yet.
+for DBoxDir in Documents; do
+    if [[ ! -e "$HOME/$DBoxDir" && ! -L "$HOME/$DBoxDir" ]]; then
+        ln -vs $HOME/Dropbox/$DBoxDir $HOME/
+    elif [[ ! -L "$HOME/$DBoxDir" ]]; then
+        echo "~/$DBoxDir exists, but is not a symlink"
+        ln -vsFi $HOME/Dropbox/$DBoxDir $HOME/
+    elif [[ $(readlink $HOME/$DBoxDir) != "$HOME/Dropbox/$DBoxDir" &&
+            $(readlink $HOME/$DBoxDir) != "Dropbox/$DBoxDir" ]]; then
+        echo "~/$DBoxDir currently points to $(readlink $HOME/$DBoxDir)"
+        ln -vsFi $HOME/Dropbox/$DBoxDir $HOME/
+    else
+        echo "~/$DBoxDir already points to Dropbox"
+    fi
+done
