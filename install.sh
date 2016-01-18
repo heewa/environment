6f7841cc6b241b263250854187839a9461b7c7e2
Heewa Barfchin <heewa.b@gmail.com>
2016-01-18 14:12:11 -0500

    install: Improve console messages

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 #
 # Set up symlinks to dotfiles & make sure directories I want are there.
 
+
+# .rc files need to be renamed individually
+echo
+echo '==] SymLinking ~/.rc files'
 ln -vsf $PWD/dotfiles/vim ~/.vim
 ln -vsf $PWD/dotfiles/vim/vimrc ~/.vimrc
 ln -vsf $PWD/dotfiles/screenrc ~/.screenrc
@@ -10,11 +14,15 @@ ln -vsf $PWD/dotfiles/gitignore ~/.gitignore
 ln -vsf $PWD/dotfiles/bashrc ~/.bashrc
 ln -vsf $PWD/dotfiles/profile ~/.profile
 
+echo
+echo '==] Creating home dirs'
 for D in src build tmp scripts .bash; do
     mkdir -v $HOME/$D
 done
 
 # Create dropbox symlinks, even if Dropbox isn't updated yet.
+echo
+echo '==] SymLinking Dropbox Dirs'
 for DBoxDir in Documents; do
     if [[ ! -e "$HOME/$DBoxDir" && ! -L "$HOME/$DBoxDir" ]]; then
         ln -vs $HOME/Dropbox/$DBoxDir $HOME/
@@ -32,3 +40,8 @@ done
 
 # git aware prompt
 git clone git://github.com/jimeh/git-aware-prompt.git $HOME/.bash/git-aware-prompt
+echo
+echo '==] Creating/Updating git aware prompt from git repo'
+
+echo
+echo '==] Done'
