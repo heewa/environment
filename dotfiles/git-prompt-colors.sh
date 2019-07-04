1185d91b62576ce7aca62f35922ba7926c4da951
Heewa Barfchin <heewa.b@gmail.com>
2019-07-04 02:15:55 -0400

    prompt: remove emojis :(

--- dotfiles/git-prompt-colors.sh
+++ dotfiles/git-prompt-colors.sh
@@ -41,7 +41,8 @@ override_git_prompt_colors() {
   # GIT_PROMPT_UPSTREAM=" {${Blue}_UPSTREAM_${ResetColor}}"
 
   ## _LAST_COMMAND_INDICATOR_ will be replaced by the appropriate GIT_PROMPT_COMMAND_OK OR GIT_PROMPT_COMMAND_FAIL
-  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_\$(CURRENT_EMOJI_RAW)$EMOJI_SUFFIX${White}${Time12a}${Yellow}${PathShort}${ResetColor}"
+  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_${White}${Time12a}${Yellow}${PathShort}${ResetColor}"
+  #GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_\$(CURRENT_EMOJI_RAW)$EMOJI_SUFFIX${White}${Time12a}${Yellow}${PathShort}${ResetColor}"
   GIT_PROMPT_START_ROOT="_LAST_COMMAND_INDICATOR_${GIT_PROMPT_START_USER}"
   GIT_PROMPT_END_USER="${ResetColor} $ "
   GIT_PROMPT_END_ROOT=" ${Red}#${ResetColor} "
