fad1f40ea6755d0e3c23d33e00a37c1ef1324eb3
Heewa Barfchin <heewa.b@gmail.com>
2019-07-04 02:52:07 -0400

    prompt: nerd fonts instead of emojis

--- dotfiles/git-prompt-colors.sh
+++ dotfiles/git-prompt-colors.sh
@@ -1,6 +1,19 @@
 override_git_prompt_colors() {
   GIT_PROMPT_THEME_NAME="Emoji"
 
+  # Nerd Font Symbols
+  NF_X=$'\uf00d'
+  NF_CHECK=$'\uf00c'
+  NF_PLUS=$'\uf067'
+  NF_FLAG=$'\uf73f'
+  NF_COMMIT_LOCAL=$'\ufc19'
+  NF_BRANCH=$'\ufb2b'
+  NF_CURVED_OPEN=$'\ue0b7'
+  NF_CURVED_CLOSE=$'\ue0b5'
+  NF_ANGLED_OPEN=$'\ue0b3'
+  NF_ANGLED_CLOSE=$'\ue0b1'
+  NF_DOTTED_VERT=$'\ue621'
+
   # Heewa: show seconds in time
   Time12a="\$(date +%H:%M:%S)"
   # PathShort="\w";
@@ -9,26 +22,30 @@ override_git_prompt_colors() {
   # GIT_PROMPT_PREFIX="["                 # start of the git info string
   # GIT_PROMPT_SUFFIX="]"                 # the end of the git info string
   # GIT_PROMPT_SEPARATOR="|"              # separates each item
+  GIT_PROMPT_PREFIX="${NF_BRANCH} "                 # start of the git info string
+  GIT_PROMPT_SUFFIX=""                 # the end of the git info string
+  GIT_PROMPT_SEPARATOR="${NF_DOTTED_VERT}"              # separates each item
 
   # GIT_PROMPT_BRANCH="${Magenta}"        # the git branch that is active in the current directory
   # GIT_PROMPT_STAGED="${Red}●"           # the number of staged files/directories
   # GIT_PROMPT_CONFLICTS="${Red}✖ "       # the number of files in conflict
+  GIT_PROMPT_CONFLICTS="${Red}${NF_X} "       # the number of files in conflict
   # GIT_PROMPT_CHANGED="${Blue}✚ "        # the number of changed files
+   GIT_PROMPT_CHANGED="${Blue}${NF_PLUS} "        # the number of changed files
 
   # GIT_PROMPT_REMOTE=" "                 # the remote branch name (if any) and the symbols for ahead and behind
   # GIT_PROMPT_UNTRACKED="${Cyan}…"       # the number of untracked files/dirs
   # GIT_PROMPT_STASHED="${BoldBlue}⚑ "    # the number of stashed files/dir
   # GIT_PROMPT_CLEAN="${BoldGreen}✔"      # a colored flag indicating a "clean" repo
+  GIT_PROMPT_STASHED="${BoldBlue}${NF_FLAG} "    # the number of stashed files/dir
+  GIT_PROMPT_CLEAN="${BoldGreen}${NF_CHECK}"      # a colored flag indicating a "clean" repo
 
   ## For the command indicator, the placeholder _LAST_COMMAND_STATE_ 
   ## will be replaced with the exit code of the last command
   ## e.g.
-  ## GIT_PROMPT_COMMAND_OK="${Green}✔-_LAST_COMMAND_STATE_ "    # indicator if the last command returned with an exit code of 0
-  ## GIT_PROMPT_COMMAND_FAIL="${Red}✘-_LAST_COMMAND_STATE_ "    # indicator if the last command returned with an exit code of other than 0
-
-  # Heewa: don't show anything on successful command
   GIT_PROMPT_COMMAND_OK=""    # indicator if the last command returned with an exit code of 0
-  GIT_PROMPT_COMMAND_FAIL="${Red}✘-_LAST_COMMAND_STATE_${ResetColor} "    # indicator if the last command returned with an exit code of other than 0
+  #GIT_PROMPT_COMMAND_FAIL="${Red}✘-_LAST_COMMAND_STATE_${ResetColor} "    # indicator if the last command returned with an exit code of other than 0
+  GIT_PROMPT_COMMAND_FAIL="${Red}${NF_X} _LAST_COMMAND_STATE_${ResetColor} "    # indicator if the last command returned with an exit code of other than 0
 
   ## template for displaying the current virtual environment
   ## use the placeholder _VIRTUALENV_ will be replaced with 
@@ -41,17 +58,18 @@ override_git_prompt_colors() {
   # GIT_PROMPT_UPSTREAM=" {${Blue}_UPSTREAM_${ResetColor}}"
 
   ## _LAST_COMMAND_INDICATOR_ will be replaced by the appropriate GIT_PROMPT_COMMAND_OK OR GIT_PROMPT_COMMAND_FAIL
-  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_${White}${Time12a}${Yellow}${PathShort}${ResetColor}"
+  GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_${White}${Time12a} ${Yellow}${PathShort}${ResetColor}"
   #GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_\$(CURRENT_EMOJI_RAW)$EMOJI_SUFFIX${White}${Time12a}${Yellow}${PathShort}${ResetColor}"
   GIT_PROMPT_START_ROOT="_LAST_COMMAND_INDICATOR_${GIT_PROMPT_START_USER}"
-  GIT_PROMPT_END_USER="${ResetColor} $ "
-  GIT_PROMPT_END_ROOT=" ${Red}#${ResetColor} "
+  GIT_PROMPT_END_USER="${ResetColor}${NF_CURVED_CLOSE} "
+  GIT_PROMPT_END_ROOT="${Red}#${ResetColor} "
 
   ## Please do not add colors to these symbols
   # GIT_PROMPT_SYMBOLS_AHEAD="↑·"             # The symbol for "n versions ahead of origin"
   # GIT_PROMPT_SYMBOLS_BEHIND="↓·"            # The symbol for "n versions behind of origin"
   # GIT_PROMPT_SYMBOLS_PREHASH=":"            # Written before hash of commit, if no name could be found
   # GIT_PROMPT_SYMBOLS_NO_REMOTE_TRACKING="L" # This symbol is written after the branch, if the branch is not tracked 
+  GIT_PROMPT_SYMBOLS_NO_REMOTE_TRACKING="${NF_COMMIT_LOCAL}" # This symbol is written after the branch, if the branch is not tracked 
 }
 
 reload_git_prompt_colors "Emoji"
