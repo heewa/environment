7e849a960aecd5d78393696d257dc9dca9f30270
Heewa Barfchin <heewa.b@gmail.com>
2022-05-07 14:48:38 -0400

    update docker perms

--- scripts/start_jellyfin.sh
+++ scripts/start_jellyfin.sh
@@ -1,2 +1,2 @@
 #/bin/bash -e
-sudo /usr/bin/docker run -v /tmp/jellyfin:/tmp -v /srv/jellyfin/config:/config -v /srv/jellyfin/cache:/cache -v /media/heewa:/media -v /home/heewa/Videos:/Videos -v /tmp/jellyfin-transcodes:/config/transcodes --net=host jellyfin/jellyfin:latest
+/usr/bin/docker run -v /tmp/jellyfin:/tmp -v /srv/jellyfin/config:/config -v /srv/jellyfin/cache:/cache -v /media/heewa:/media -v /home/heewa/Videos:/Videos -v /tmp/jellyfin-transcodes:/config/transcodes --net=host jellyfin/jellyfin:latest
--- scripts/start_transmission-rss.sh
+++ scripts/start_transmission-rss.sh
@@ -1,2 +1,2 @@
 #/bin/bash -e
-sudo docker run -t -v ~/.config/transmission-rss/config.yml:/etc/transmission-rss.conf -v ~/.config/transmission-rss/seen:/var/lib/transmission-rss/seen nning2/transmission-rss
+docker run -t -v ~/.config/transmission-rss/config.yml:/etc/transmission-rss.conf -v ~/.config/transmission-rss/seen:/var/lib/transmission-rss/seen nning2/transmission-rss
