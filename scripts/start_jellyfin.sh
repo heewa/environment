c2a05958dd50668d9cb1cd4c8ec85de5b9bbb9fe
Heewa Barfchin <heewa.b@gmail.com>
2022-11-18 18:18:29 -0500

    update jellyfin

--- scripts/start_jellyfin.sh
+++ scripts/start_jellyfin.sh
@@ -1,2 +1,2 @@
 #/bin/bash -e
-/usr/bin/docker run -v /tmp/jellyfin:/tmp -v /srv/jellyfin/config:/config -v /srv/jellyfin/cache:/cache -v /media/heewa:/media -v /home/heewa/Videos:/Videos -v /tmp/jellyfin-transcodes:/config/transcodes --net=host jellyfin/jellyfin:latest
+/usr/bin/docker run -v /tmp/jellyfin:/tmp -v /srv/jellyfin/config:/config -v /srv/jellyfin/cache:/cache -v /home/heewa/Videos:/Videos -v /tmp/jellyfin-transcodes:/config/transcodes -p 8096:8096 -p 8920:8920 --device /dev/fb0:/dev/fb0 'jellyfin/jellyfin:latest'
