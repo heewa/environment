206c046927847c2254ffe115fd24eea3c7f99a70
Heewa Barfchin <heewa@barfch.in>
2023-11-27 10:17:12 -0500

    script: clean up old zig releases

--- misc/zig/update_zig.sh
+++ misc/zig/update_zig.sh
@@ -4,6 +4,7 @@ SCRIPT_DIR="$(realpath -s -- "$PWD/$(dirname -- "$0")")"
 NIGHTLY_DIR="$SCRIPT_DIR/nightly"
 mkdir -p "$NIGHTLY_DIR"
 
+# Get latest
 DOWNLOAD_INDEX='https://ziglang.org/download/index.json'
 LATEST_URL="$(curl -Ls "$DOWNLOAD_INDEX" | jq --raw-output '.master["x86_64-linux"].tarball')"
 LATEST_VER="$(basename -s .tar.xz "$LATEST_URL")"
@@ -13,4 +14,20 @@ if [[ ! -d "$LATEST_DIR" ]]; then
 else
     echo "Already have $LATEST_VER"
 fi
+
+# Link zig to latest
 ln -sf --no-target-directory "$LATEST_DIR" "$NIGHTLY_DIR/latest"
+
+# Clean up every version except latest 2
+VERSIONS="$(ls -t -I latest "$NIGHTLY_DIR" | tail -n +3)"
+if [[ "$VERSIONS" != "" ]]; then
+    echo "Cleaning up $(echo "$VERSIONS" | wc -l) older versions, taking up $(cd $NIGHTLY_DIR && du -shc $VERSIONS | tail -n1 | cut -f1)"
+
+    for VER in $VERSIONS; do
+        D="$NIGHTLY_DIR"/"$VER"
+        SIZE="$(du -sh "$D" | cut -f1)"
+        MTIME="$(stat -c %y "$D")"
+        echo "$SIZE $MTIME $VER"
+        rm -rf "$D"
+    done
+fi
